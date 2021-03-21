4b8b470ea5851d7b9dc2d88b4f0410dce703af4e
MaTtHeW-S52 <77634517+MaTtHeW-S52@users.noreply.github.com>
2021-03-21 16:35:46 +0000

    functioning city report

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -126,6 +126,48 @@ public class App
             System.out.println("Failed to get country details");
         }
     }
+
+    public void cityReport()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT Name, CountryCode, District, Population "
+                            + "FROM city "
+                            + "ORDER BY Population DESC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            while (rset.next())
+            {
+                City city = new City();
+                city.city_name = rset.getString("Name");
+                city.city_country_code = rset.getString("CountryCode");
+                city.city_district = rset.getString("District");
+                city.city_population = rset.getInt("Population");
+
+
+                //if the data is present
+
+                //Show the result on screen
+                System.out.println(
+                        city.city_name+ " "
+                                + city.city_country_code + " "
+                                + city.city_district + " "
+                                + city.city_population + "\n"
+                );
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get city details");
+        }
+    }
 /**
     public void displayCountry(Country emp)
     {
@@ -162,6 +204,7 @@ public class App
 
         // Get Employee
          a.countryReport();
+         a.cityReport();
         // Display results
         //a.displayCountry(emp);
 

4b27cad9e414b5582e02903cff4a6cd51db1941a
Sarevok <Mimmius89@outlook.it>
2021-03-22 16:51:36 +0100

    Revert

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -21,7 +21,6 @@ public class App
     DataConnect a = new DataConnect();
     Req1 first = new Req1(a);
     Req7 seven = new Req7(a);
-    Req11 eleven = new Req11(a);
 
 
     public static void main(String[] args)
@@ -35,7 +34,6 @@ public class App
         // Get Data
         a.first.countryReport();
         a.seven.cityReport();
-        a.eleven.cityReport();
 
         // Disconnect from database
         a.a.disconnect();

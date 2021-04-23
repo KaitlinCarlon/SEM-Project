50b9f1d48685e91d5358dbda0a3d2e39e9eaa562
Sarevok <Mimmius89@outlook.it>
2021-04-23 15:33:11 +0200

    Try

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -150,7 +150,77 @@ public class AppIntegrationTest
     }
 
     @Test
-    void disconnect(){
+    void Language(){
+        //home build
+        /**
+         * Chinese 1191843539 19.61
+         * Hindi 405633085 6.67
+         * Spanish 355029461 5.84
+         * English 347077860 5.71
+         * Arabic 233839240 3.85
+         */
+        //travis result
+        /**
+         * Chinese 1191843539 19.61
+         * Hindi 405633070 6.67
+         * Spanish 355029462 5.84
+         * English 347077867 5.71
+         * Arabic 233839238 3.85
+         */
+
+        ReqLanguage a = new ReqLanguage(app);
+        Language b = a.Ritorna();
+
+        assertEquals(b.Lingua(), "Arabic");
+        assertEquals(b.Parlare(), 233839238);
+        assertEquals(b.Percentuale(), 3.85f);
+    }
+
+    @Test
+    void extraPopulation(){
+        /**
+         * 6078749450
+         *
+         * 3705025700
+         *
+         * 38140000
+         *
+         * 37032000
+         *
+         * 360593
+         *
+         * 440900
+         */
+
+        //Population
+        ExtraPopReq a = new ExtraPopReq(app);
+
+        a.totalPop(Location.Basic);
+        long b = (long) a.Ritorna();
+        assertEquals(6078749450L, b);
+        a.totalPop(Location.Continent);
+        long c = (long) a.Ritorna();
+        assertEquals(3705025700L, c);
+        a.totalPop(Location.Region);
+        long d = (long) a.Ritorna();
+        assertEquals(38140000, d);
+        a.totalPop(Location.Country);
+        long e = (long) a.Ritorna();
+        assertEquals(37032000, e);
+        a.totalPop(Location.District);
+        long f = (long) a.Ritorna();
+        assertEquals(360593, f);
+        a.totalPop(Location.City);
+        long g = (long) a.Ritorna();
+        assertEquals(440900, g);
+
+
         app.disconnect();
+    }
+
+
+    @Test
+    void disconnect(){
+
     }
 }
